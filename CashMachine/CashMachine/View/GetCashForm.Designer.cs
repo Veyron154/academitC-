678e7e30243b46d30101ef4de667325bba34baad
Veyron154 <De1ete54@mail.ru>
2016-08-09 23:09:05 +0600

    добавлено срабатывание по Enter

--- CashMachine/CashMachine/View/GetCashForm.Designer.cs
+++ CashMachine/CashMachine/View/GetCashForm.Designer.cs
@@ -92,6 +92,7 @@
             // 
             // GetCashForm
             // 
+            this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(409, 111);
